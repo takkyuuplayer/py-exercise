ce2945df97f143b0c83eb9c0788f615a29fea2c9
takkyuuplayer <takkyuuplayer@gmail.com>
2017-09-24 07:31:50 +0900

    Add test_zeros and argmax

--- tests/numpy_test.py
+++ tests/numpy_test.py
@@ -10,5 +10,19 @@ class TesNumpy(unittest.TestCase):
         self.assertEqual(tuple, type(numpy.random.rand(2,3, 4).shape))
         self.assertEqual((2, 3, 4), numpy.random.rand(2,3, 4).shape)
 
+    def test_zeros(self):
+        self.assertEqual(numpy.ndarray, type(numpy.zeros(5)))
+        self.assertEqual((5, ), numpy.zeros(5).shape)
+
+    def test_argmax(self):
+        arr = numpy.array([1, 3, 5, 0, 9, 2])
+
+        self.assertEqual(numpy.ndarray, type(arr))
+        self.assertEqual(4, numpy.argmax(arr))
+
+        arr2 = numpy.array([[1,9,3,5], [2,8,4,6]])
+
+        self.assertEqual([1, 0, 1, 1], numpy.argmax(arr2, axis=0).tolist())
+
 if __name__ == '__main__':
     unittest.main()
